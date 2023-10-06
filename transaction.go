4fc112e0b36bdd9c107c213db82dbbeef08d7265
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2023-10-06 22:53:08 +0900

    feat: add comments

--- transaction.go
+++ transaction.go
@@ -7,10 +7,12 @@ import (
 	"github.com/pkg/errors"
 )
 
+// TxStarter is an interface to start a transaction.
 type TxStarter interface {
 	BeginTx(ctx context.Context, opts *sql.TxOptions) (*sql.Tx, error)
 }
 
+// Transact is a helper function to execute a function in a transaction.
 func Transact(ctx context.Context, starter TxStarter, f func(context.Context, *sql.Tx) error) (err error) {
 	var tx *sql.Tx
 	if tx, err = starter.BeginTx(ctx, nil); err != nil {
